87e0850ebaa899c2f96ef8d6ff090e5fcadd8708
Esteban <matatron@gmail.com>
2017-11-29 19:27:03 -0600

    fixes

--- assets/app/app.js
+++ assets/app/app.js
@@ -3,15 +3,20 @@ webApp = angular.module('Enigmaster', [
 	'ui.grid'
 ]).filter('clock', function() {
     return function(date) {
+        var prefix = '';
         function pad(n) {
             return (n<10)?"0"+n:n;
         }
         date = new Date(date);
-        
-        var s = date.getTime()/1000;
+        date = date.getTime();
+        if (date<0) {
+            prefix = '-';
+            date = -date;
+        }
+        var s = date/1000;
         var h = Math.floor(s/3600);
         var m = Math.floor(s/60)%60;
         var s = Math.round(s%60);
-        return h+":"+pad(m)+":"+pad(s);
+        return prefix+h+":"+pad(m)+":"+pad(s);
     };
 });
