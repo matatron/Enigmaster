75b5f096168a6c35b672487af1b488c2c3b140c3
Esteban <matatron@gmail.com>
2017-11-29 19:38:18 -0600

    Fix

--- assets/app/PlayerviewController.js
+++ assets/app/PlayerviewController.js
@@ -1,6 +1,23 @@
 webApp = angular.module('Enigmaster', [
-]);
-webApp.controller('PlayerviewController', ['$scope', '$http', '$timeout', '$interval', '$filter', function($scope, $http, $timeout, $interval, $filter) {
+]).filter('clock', function() {
+    return function(date) {
+        var prefix = '';
+        function pad(n) {
+            return (n<10)?"0"+n:n;
+        }
+        date = new Date(date);
+        date = date.getTime();
+        if (date<0) {
+            prefix = '-';
+            date = -date;
+        }
+        var s = date/1000;
+        var h = Math.floor(s/3600);
+        var m = Math.floor(s/60)%60;
+        var s = Math.round(s%60);
+        return prefix+h+":"+pad(m)+":"+pad(s);
+    };
+}).controller('PlayerviewController', ['$scope', '$http', '$timeout', '$interval', '$filter', function($scope, $http, $timeout, $interval, $filter) {
     $scope.data={};
     $scope.data.status = 0;
     $scope.data.end = 0;
