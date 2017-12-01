b07fb1802a0aa4721cb1596f21eed9fac8bde8a1
Esteban <matatron@gmail.com>
2017-11-30 22:38:40 -0600

    Fix en tiempos de jugadores

--- application/views/playerviews/basic.php
+++ application/views/playerviews/basic.php
@@ -7,14 +7,14 @@
 </div>
 <div ng-if="data.status == 2">
     <div class="lcd giant-text">
-        {{timeLeft | clock}}
+        {{timeLeft-punishment*60000 | clock}}
     </div>
     <div class="small-text">
         Pistas usadas: 
         <i class="fa fa-key" ng-repeat="n in pistas"></i> <span ng-if="pistas.length>0" class="lcd">= {{punishment}}:00</span>
     </div>
-    <div class="lcd small-text">
-        {{timeLeft-punishment*60000 | clock}}
+    <div class="small-text">
+        Tiempo transcurrido: <span class="lcd ">{{timePass | clock}}</span>
     </div>
     <div class="font-diogenes" style="width: 60%; margin: 0 auto;">
         {{clue}}
