1d1b00188edd58a1d80006efc5e58ab46616b48f
Esteban <matatron@gmail.com>
2017-11-30 23:13:39 -0600

    llamas...

--- application/views/playerviews/basic.php
+++ application/views/playerviews/basic.php
@@ -12,15 +12,15 @@
     <div class="lcd giant-text">
         {{timeLeft-punishment*60000 | clock}}
     </div>
-    <div>
-        <img src="/assets/images/flama.webp" ng-repeat="n in counters" class="flame">
-    </div>
     <div class="small-text">
         Pistas usadas: 
         <i class="fa fa-key" ng-repeat="n in pistas"></i> <span ng-if="pistas.length>0" class="lcd">= {{punishment}}:00</span>
     </div>
     <div class="small-text">
         Tiempo transcurrido: <span class="lcd ">{{timePass | clock}}</span>
+    </div>
+    <div>
+        <img src="/assets/images/flama.webp" ng-repeat="n in counters" class="flame">
     </div>
     <div class="font-diogenes" style="width: 60%; margin: 0 auto;">
         {{clue}}
