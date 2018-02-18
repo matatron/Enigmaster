bc5f65973397178533ef376a0ff1d9c655ff8615
Esteban <matatron@gmail.com>
2018-02-18 15:27:03 -0600

    gizmos binario

--- application/classes/Controller/Gizmo.php
+++ application/classes/Controller/Gizmo.php
@@ -38,7 +38,7 @@ class Controller_Gizmo extends Controller {
             if ($group->loaded()) {
                 if ($group->status == 3) { // not started
                     //$this->response->body("0\r");
-                    echo "0\r";
+                    echo "on\r";
                 } else {
                     if ($hasChanged) {
                         foreach($rules as $rule) {
@@ -63,6 +63,79 @@ class Controller_Gizmo extends Controller {
                     }
                     //$this->response->body($group->progress."\r");
                     echo $group->progress."\r";
+                }
+            } else {
+                //$this->response->body("off\r");
+                echo "off\r";
+            }
+        }
+        die();
+    }
+
+    public function action_reportbin()
+    {
+        $hasChanged = false;
+        $query = $this->request->query();
+        $gizmoId = $this->request->param('id');
+        $data = json_encode($query);
+        $gizmo = ORM::factory('Gizmo')->where('uid', '=', $gizmoId)->find();
+        if ($gizmo->loaded()) {
+            $gizmo->lastActive = time();
+            if ($data != $gizmo->data) {
+                $gizmo->prevdata = $gizmo->data;
+                $gizmo->data = $data;
+                $hasChanged = true;
+            }
+            $gizmo->save();
+        }else{
+            $hasChanged = true;
+            $gizmo = ORM::factory('Gizmo');
+            $gizmo->uid = $gizmoId;
+            $gizmo->name = 'Gizmo '.$gizmoId;
+            $gizmo->description = '';
+            $gizmo->data = $data;
+            $gizmo->lastActive = time();
+            $gizmo->save();
+        }
+
+        $rules = json_decode($gizmo->ifttt);
+
+        if($gizmo->room) {
+            $group = ORM::factory('Group')
+                ->where('room_id', '=', $gizmo->room)
+                ->and_where('status', '>', 0)
+                ->find();
+            if ($group->loaded()) {
+                if ($group->status == 3) { // not started
+                    //$this->response->body("0\r");
+                    echo "on\r";
+                } else {
+                    if ($hasChanged) {
+                        foreach($rules as $rule) {
+                            if (isset($query[$rule->if]) &&  $query[$rule->if] == $rule->this) {
+                                switch ($rule->then) {
+                                    case "progress":
+                                        if ($group->progress < $rule->that) {
+                                            $group->progress = $rule->that;
+                                            $group->save();
+                                        }
+                                        break;
+                                    case "endgame":
+                                        $group->endgame(($rule->that == "lost"));
+                                        break;
+                                    case "punish":
+                                        $group->punish += $rule->that;
+                                        break;
+
+                                }
+                            }
+                        }
+                    }
+                    $puzzles = json_decode($group->puzzles);
+                    foreach($puzzles as $p) {
+                        echo 0+$p->complete;
+                    }
+                    echo "\r\n";
                 }
             } else {
                 //$this->response->body("off\r");
