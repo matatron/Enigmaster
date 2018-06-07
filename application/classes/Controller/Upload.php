d9197de8699de392d689bc312edb24e068491670
Esteban <matatron@gmail.com>
2018-06-06 23:18:45 -0600

    Fixes

--- application/classes/Controller/Upload.php
+++ application/classes/Controller/Upload.php
@@ -5,16 +5,19 @@ class Controller_Upload extends Controller_Website {
     public function action_index()
     {
 
+        $message = "";
         if ($this->request->method() == Request::POST)
         {
+            $message = "Datos recibidos: ".$_FILES['video']["name"];
             if (isset($_FILES['video']))
             {
-                move_uploaded_file($_FILES['video']["tmp_name"], DOCROOT.'assets/video/videoGrupo.mp4');
+                move_uploaded_file($_FILES['video']["tmp_name"], 'assets/video/videoGrupo.mp4');
+                $message = "Video subido";
             }
         }
 
         $this->template->title = 'Subir video';
-        $this->template->content = View::factory('upload/index');
+        $this->template->content = View::factory('upload/index')->bind("message", $message);
 
     }
 
--- application/views/upload/index.php
+++ application/views/upload/index.php
@@ -1,3 +1,4 @@
+<h1><?=$message;?></h1>
 <form id="upload-form" action="<?php echo URL::site('upload/index') ?>" method="post" enctype="multipart/form-data">
     <p>Choose file:</p>
     <p><input type="file" name="video" id="video" /></p>
