d148d486e106a7d12baed0ed8dd487dbaf737597
Esteban <matatron@gmail.com>
2018-06-16 19:07:52 -0600

    condition fix

--- application/classes/Controller/Playerview.php
+++ application/classes/Controller/Playerview.php
@@ -33,7 +33,7 @@ class Controller_Playerview extends Controller_Template {
                 $lastMusic = $puzzle->music;
             }
             $music[] = $lastMusic;
-            $sound[] = $puzzle->sound || '';
+            $sound[] = (isset($puzzle->music))? $puzzle->sound : '';
         }
 
         $this->template->roomId = $roomId;
