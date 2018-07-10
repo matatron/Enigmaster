a6562ced0cdc24746a78fa56e43d489a471f02a3
Esteban <matatron@gmail.com>
2018-07-09 22:20:37 -0600

    font size

--- application/views/rooms/lasttime.php
+++ application/views/rooms/lasttime.php
@@ -1,5 +1,5 @@
-<div style="font-size: 2em;">
 <div><?= $group->team_name; ?></div>
+<div style="font-size: 2em;">
 <div class="lcd giant-text"><?php
     $minutos = floor($group->time/60);
     $segundos = $group->time%60;
