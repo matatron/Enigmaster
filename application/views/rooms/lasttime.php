0b72b4734a51d2fd850c9e1c6c094b644f5ae875
Esteban <matatron@gmail.com>
2018-07-16 17:18:51 -0600

    Fix segundos

--- application/views/rooms/lasttime.php
+++ application/views/rooms/lasttime.php
@@ -5,6 +5,7 @@
 <div class="lcd giant-text"><?php
     $minutos = floor($group->time/60);
     $segundos = $group->time%60;
+    if ($segundos<10) $segundos = "0".$segundos;
     echo $minutos.":".$segundos;
 ?></div>
 </div>
