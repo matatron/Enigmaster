6f9e8c6e1f184116f0d781f361af9c3a29b22f9f
Esteban Mata <matatron@gmail.com>
2019-09-04 01:56:50 -0600

    Create pi3.php

---
+++ application/views/playerviews/pi3.php
@@ -0,0 +1,23 @@
+<style>
+    html {
+        height: 100%;
+    }
+    body {
+        height: 100%;
+        margin: 0 !important;
+        background-image: url('/assets/images/fondo_pi1_1.jpg');
+        background-size: contain;
+    }
+</style>
+<div ng-controller="PlayerviewControllerPi1" ng-init="roomId = <?=$roomId; ?>" >
+    <div class="text"></div>
+    <div ng-if="data.status == 3">
+    </div>
+    <div ng-if="data.status == 2 || data.status == 1">
+        <video style="width: 100%; height: 75vw;" id="tvvideo" autoplay></video>
+    </div>
+    <div ng-if="data.status == 1">
+    </div>
+    <div ng-if="data.status == 0">
+    </div>
+</div>
